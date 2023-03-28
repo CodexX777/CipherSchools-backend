c276d7376c2be820d54b019c65fd22e6b146e710
CodexX777 <shivang260279@gmail.com>
2023-03-28 19:55:18 +0530

    added all the controllers required for user info updation along with image upload in S3 bucket

--- controllers/profileUpdate-controller.js
+++ controllers/profileUpdate-controller.js
@@ -1,6 +1,11 @@
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
+const { v4: uuidv4 } = require("uuid");
+const AWS = require('aws-sdk');
+const s3 =new AWS.S3();
+
+
 
 const aboutUpdate = async (req, res, next) => {
   const userId = req.params.uid;
@@ -100,12 +105,100 @@ const educationUpdate = async (req, res, next) => {
 
   await user.save();
 
-
   res.status(201).json({
-    message:"info updated successfully."
+    message: "info updated successfully.",
   });
 };
 
+const interestUpdate = async (req, res, next) => {
+  const userId = req.params.uid;
+
+  const { options } = req.body;
+  let user;
+  try {
+    user = await User.findById(userId);
+  } catch (error) {
+    return next(
+      new HttpError("Something went wrong, please try again later.", 500)
+    );
+  }
+
+  if (!user) {
+    return next(
+      new HttpError(
+        "Could not identify user, Credentials seem to be wrong.",
+        401
+      )
+    );
+  }
+
+  user.Interests = options;
+  await user.save();
+
+  res.status(201).json({
+    message: "interests updated successfully.",
+  });
+};
+
+const userInfoUpdate = async (req, res, next) => {
+  const { FirstName, LastName, Email, PhoneNo, file } = req.body;
+  const userId = req.params.uid;
+
+
+  const filename = uuidv4();
+
+  let user;
+  try {
+    user = await User.findById(userId);
+  } catch (error) {
+    return next(
+      new HttpError("Something went wrong, please try again later.", 500)
+    );
+  }
+
+  if (!user) {
+    return next(
+      new HttpError(
+        "Could not identify user, Credentials seem to be wrong.",
+        401
+      )
+    );
+  }
+
+  try {
+    await s3
+      .putObject({
+        Body: req.file.buffer,
+        Bucket: process.env.CYCLIC_BUCKET_NAME,
+        Key: filename,
+        ContentType: req.file.mimetype,
+      })
+      .promise();
+
+    // console.log("success");
+  } catch (error) {
+    // console.log(error);
+    return next(
+      new HttpError(
+        "Something went wrong in Aws , please try again later.",
+        500
+      )
+    );
+  }
+
+  user.FirstName = FirstName;
+  user.LastName = LastName;
+  user.Email = Email;
+  user.PhoneNo = PhoneNo;
+  user.ProfilePic=filename;
+  await user.save();
+  res.status(201).json({
+    message: "info updated successfully.",
+  });
+};
+
+exports.interestUpdate = interestUpdate;
+exports.userInfoUpdate = userInfoUpdate;
 exports.educationUpdate = educationUpdate;
 exports.aboutUpdate = aboutUpdate;
 exports.socialsUpdate = socialsUpdate;
