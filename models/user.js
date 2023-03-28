ce77de7a14ffbe558aa79f6c2feadbfc9c42c6a5
CodexX777 <shivang260279@gmail.com>
2023-03-28 17:39:22 +0530

    fixed a typo in user models and added a httperror model to handle errors

--- models/user.js
+++ models/user.js
@@ -5,10 +5,10 @@ const uniqueValidator=require('mongoose-unique-validator');
 
 const userSchema=new Schema({
     FirstName: {type:String, required:true},
-    LastName:{type:String, required:true},
+    LastName:{type:String, required:false},
     PhoneNo: {type:Number, required:true, minlength:10},
     Email: {type:String, required:true,unique:true},
-    password: {type:String , required:true, minlength:6},
+    Password: {type:String , required:true, minlength:6},
     followers: [{type:mongoose.Types.ObjectId, required:true,ref:'User'}],
     Links:[{type:String, required:false}],
     Interests:[{name:{type:String, required:true},id:{type:Number, required:true}}],
